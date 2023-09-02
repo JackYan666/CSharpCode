d3f49b83811748a74dbbf40b22f3e763e1028ec3
hexi.yan <8224101486@qq.com>
2023-09-02 19:05:26 +0800

    01.For循环删除集合元素 优化

--- CSharpCode.cs
+++ CSharpCode.cs
@@ -38,11 +38,13 @@ public class CSharpCode : MonoBehaviour
         //        list.ForEach(p => { Debug.LogWarning(p); });
 
 
-        //从后面忘前删除
+        //从后面往前删除
         List<string> list = new List<string>() { "red", "green", "blue", "blue", "gray", "yellow", "white", "orange" };
         for (int i = list.Count-1; i >=0 ; i--)
         {
-            if (list[i].Equals("blue"))
+            //if (list[i].Equals("blue"))
+            //优化：常量在前面 后面的值可能为空会报异常
+            if ("blue".Equals(list[i]))
             {
                 list.Remove("blue");
             }
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # CSharpCode
+
+##01.For循环删除集合元素
+        //从后面往前删除
+        List<string> list = new List<string>() { "red", "green", "blue", "blue", "gray", "yellow", "white", "orange" };
+        for (int i = list.Count-1; i >=0 ; i--)
+        {
+            //if (list[i].Equals("blue"))
+            //优化：常量在前面 后面的值可能为空会报异常
+            if ("blue".Equals(list[i]))
+            {
+                list.Remove("blue");
+            }
+        }
+        list.ForEach(p => { Debug.LogWarning(p); });
